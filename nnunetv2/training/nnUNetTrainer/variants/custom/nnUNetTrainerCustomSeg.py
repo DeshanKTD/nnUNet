7cb4bd20cd900376ad27783beab004074a86a31e
DeshanKTD <deshankalupahana@gmail.com>
2025-05-10 13:43:47 +1000

    customSeg trainer adpated to nnUNetv2

--- nnunetv2/training/nnUNetTrainer/variants/custom/nnUNetTrainerCustomSeg.py
+++ nnunetv2/training/nnUNetTrainer/variants/custom/nnUNetTrainerCustomSeg.py
@@ -1,10 +1,10 @@
 from nnunetv2.training.loss.compound_losses import DC_and_CE_loss
 from nnunetv2.training.nnUNetTrainer.variants.network_architecture.nnUNetTrainerNoDeepSupervision import nnUNetTrainerNoDeepSupervision
+import torch
 
 
 class nnUNetTrainerCustomSeg(nnUNetTrainerNoDeepSupervision):
-    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
-                 unpack_data=True, deterministic=True, fp16=False):
-        super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
-                         deterministic, fp16)
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.loss = DC_and_CE_loss({'batch_dice': self.batch_dice, 'smooth': 0, 'do_bg': False}, {})
